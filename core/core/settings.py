f21a9944bf0be6421068d7c74875117ee177a0a9
Ariwsb14 <Ariasb1385@gmail.com>
2025-02-05 13:30:22 +0330

    making media , static and templlates roots
    +static files have been added
    +media file
    +templates

--- core/core/settings.py
+++ core/core/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,8 +118,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    BASE_DIR / "statics",
+]
+
+#media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#config for user auth model
+"""
+AUTH_USER_MODEL = 'accounts.User'
+LOGIN_REDIRECT_URL = ''
+"""
