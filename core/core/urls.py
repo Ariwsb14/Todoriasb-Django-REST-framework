42fe9b1f2357f10a740c5390abaa312baaea59bd
Ariwsb14 <Ariasb1385@gmail.com>
2025-02-06 20:03:27 +0330

    downloadable .json apidoc added
    + url: /swagger/api.json

--- core/core/urls.py
+++ core/core/urls.py
@@ -24,9 +24,9 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="todolist API",
       default_version='v1',
-      description="Test description",
+      description="todoriasb api",
       terms_of_service="https://www.google.com/policies/terms/",
       contact=openapi.Contact(email="contact@snippets.local"),
       license=openapi.License(name="BSD License"),
@@ -39,7 +39,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('' , include('todolist.urls')),
     path('accounts/', include('accounts.urls')),
-    #path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/api.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
