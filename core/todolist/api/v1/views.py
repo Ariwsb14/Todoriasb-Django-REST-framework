293a917d4a4183b24515cb4f7d4e608c46fd72e0
Ariwsb14 <Ariasb1385@gmail.com>
2025-02-07 20:23:56 +0330

    Update todolist/api/v1views.py for showing tasks only when user taskowner is

--- core/todolist/api/v1/views.py
+++ core/todolist/api/v1/views.py
@@ -5,19 +5,19 @@ from .serializers import TaskModelSerializer
 from .paginations import TaskPaginator
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter , OrderingFilter
-
+from accounts.models import Profile
+from django.shortcuts import redirect
 
 class TasktModelsViewSet(viewsets.ModelViewSet):
     model = Task
-    def get_queryset(self):
-        queryset = Task.objects.filter(user__id = self.request.user.id)
-        
-        return queryset
-   
-    permissions_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
     serializer_class = TaskModelSerializer
     pagination_class = TaskPaginator
     filter_backends = [DjangoFilterBackend , SearchFilter,OrderingFilter]
     filterset_fields = ['completed','user']
     search_fields = ['title']
     ordering_fields = ['created_date']
+    def get_queryset(self):
+        user = Profile.objects.get(user__id = self.request.user.id)
+        queryset = Task.objects.filter(user__id = user.id)
+        return queryset
